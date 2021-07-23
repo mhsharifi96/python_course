fa3d4606ce3c82f496493bd1c953f233928ef4d8
mohammad hossein sharifi <mh.sh7676@gmail.com>
2021-07-23 08:14:49 +0430

    add

--- customuser/accounts/urls.py
+++ customuser/accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import SignUpView,simpleLogin
+from .views import SignUpView,simpleLogin,LoginRequiredTest,testLogin,updateTestView
 
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('test/', LoginRequiredTest.as_view(), name='login-test'),
     path('simple-login/', simpleLogin, name='simple-login'),
+    path('test2/', testLogin, name='testLogin'),
+    path('simple-update/<int:pk>/<str:type>', updateTestView.as_view()),
 ]
--- customuser/accounts/views.py
+++ customuser/accounts/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render,HttpResponse,redirect
 from django.contrib.auth import authenticate,login
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 from django.urls import reverse_lazy,reverse
-from django.views.generic import CreateView
+from django.views import View
+
+from django.views.generic import CreateView,TemplateView
 from .forms import CustomUserCreationForm
+
+from django.views.generic.edit import UpdateView, DeleteView
+from django.contrib.auth.decorators import login_required
 
 
 class SignUpView(CreateView):
@@ -26,3 +32,33 @@ def simpleLogin(request):
 
     else :
         return render(request, 'simpleLogin.html',{})
+
+
+
+
+class LoginRequiredTest(LoginRequiredMixin,TemplateView):
+    template_name='test.html'
+
+
+@login_required
+def testLogin(request):
+
+    return render(request, 'test.html')
+
+from django.utils.html import escape
+
+
+class updateTestView(View):
+    def get(self,request,*args, **kwargs):
+        print(self.kwargs['pk'])
+        print(request.user)
+        # پیدا کردن ایتم مورد نظر
+        # test.objects.get(....)
+        return render(request, 'test.html')
+    def post(self,request,*args, **kwargs):
+        # دریافت درخواست
+        # پاس دادن درخواست به فرم
+        # اگر فرم صحیح بود(is_valid)
+        # سیو کردن آن
+        # ریدایرکت آن / ارسال پیام موفقیت 
+        pass
