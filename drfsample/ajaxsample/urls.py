090331d0ab495b670386c9e7da0282b89b74954f
mohammad hossein sharifi <mh.sh7676@gmail.com>
2021-07-25 20:26:38 +0430

    code ajax on class

--- drfsample/ajaxsample/urls.py
+++ drfsample/ajaxsample/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import ajaxSample,ajaxDesc,apiSample
+from .views import ajaxSample,ajaxDesc,apiSample,jsonView
 
 urlpatterns = [
     path('',ajaxDesc, name='ajax-desc'),
+    path('json',jsonView, name='json-sample'),
     path('sample',ajaxSample, name='ajax-sample'),
     path('api',apiSample, name='api-sample'),
 ]
--- drfsample/ajaxsample/views.py
+++ drfsample/ajaxsample/views.py
@@ -13,24 +13,49 @@ def ajaxDesc(request):
 
     return render(request,'index.html',{})
 
+from pprint import pprint
+
+# def ajaxSample(request):
+#     if request.method == 'POST':
+#         pprint(dict(request.POST.items()))
+#         print(request.POST['inputText1'])
+#         print(request.POST['sampleObj'])
+#         print(type(request.POST['sampleObj']))
+
+
+#     return render(request,'sample.html',{})
 
 def ajaxSample(request):
     if request.method == 'POST'  and request.is_ajax():
-        print(dict(request.POST.items()))
-        # print(request.is_ajax())
-        username = request.POST['username']
-        message = "HI " + username + ' now Time : '+ str(datetime.now())
-        polls = Poll.objects.all()
-        if username : 
-            print('test if')
-            polls = polls.filter(question__contains = username)
-        print()
+        print(dict(request.POST.items())) # محتویات درخواست مشاهده کنید
+        # print(request.is_ajax())  #print True
+        inputText = request.POST['inputText']
+        
+        polls = Poll.objects.all() #new
+        if inputText : 
+            # print('test if')
+            polls = polls.filter(question__contains = inputText)
+
+
+        message = "HI " + inputText + ' now Time : '+ str(datetime.now())
         return JsonResponse({
             'message':message,
+            # 'polls':polls   #error
             'polls':list(polls.values())
         })
 
     return render(request,'sample.html',{})
+
+def jsonView(request):
+    # نمونه تستی json 
+    return JsonResponse({
+        'message' : "this is for test :)"+str(datetime.now()),
+        'test3' : "test3",
+        'testList' : [1,2,3,4],
+        'testobj' : {'numbers':[1,2,3,4,5,6],'maktab':'sharif'}
+
+    })
+
 
 
 @csrf_exempt
